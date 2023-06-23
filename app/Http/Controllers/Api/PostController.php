3fa20392d9b39c2c9b972b73ee628d7853cf7052
Luis Parrado <luisprmat@gmail.com>
2023-06-23 00:52:19 -0500

    Use Eloquent API Resources to transform fields

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return Post::all();
+        return PostResource::collection(Post::all());
     }
 }

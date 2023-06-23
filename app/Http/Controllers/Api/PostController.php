8289045b184350fae1fb30c87f805d55da9a73ea
Luis Parrado <luisprmat@gmail.com>
2023-06-23 00:20:27 -0500

    Post DB structure and API and factory

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return Post::all();
+    }
+}
